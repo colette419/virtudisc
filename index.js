1d7cd9e5255e16962332ad89e1e48776ee9a1ab3
Ashlee <ashleemariebrown93@gmail.com>
2016-01-18 21:58:23 +0100

    added slide functionality to city images

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
+// $(document).ready(function (){
+// 	// var button = $('#a-right')
+// 	// var gallery = $('.gallery'); 
+// 	// var currentPictureIndex = 0;
+// 	// $(gallery[currentPictureIndex]).show();
+
+// 	// $('#a-right').click(function () {
+// 	// 	$(gallery[currentPictureIndex]).fadeOut(200, function(){
+		
+// 	// 	if(currentPictureIndex === gallery.length - 1){
+// 	// 		currentPictureIndex = 0;
+// 	// 	} else{
+// 	// 		currentPictureIndex++;
+// 	// 	}
+// 	// 	$(gallery[currentPictureIndex]).fadeIn(100);
+// 	// 	});
+// 	// })
+
+// 	var button = $('#a-left')
+// 	var gallery = $('.gallery'); 
+// 	var currentPictureIndex = 0;
+// 	$(gallery[currentPictureIndex]).show();
+
+// 	$('#a-left').click(function () {
+// 		$(gallery[currentPictureIndex]).fadeOut(200, function(){
+		
+// 		if(currentPictureIndex === 0){
+// 			currentPictureIndex = gallery.length -1 ;
+// 		} else{
+// 			currentPictureIndex--;
+// 		}
+// 		$(gallery[currentPictureIndex]).fadeIn(100);
+// 		});
+// 	})
+// });
+
+
+//If the right button is clicked, do this
+//Else if the left button is clicked do this
+
+
 $(document).ready(function (){
-	var gallery = $('.images'); 
+	var buttonr = $('#a-right');
+	var buttonl = $('#a-left')
+	var gallery = $('.gallery'); 
 	var currentPictureIndex = 0;
 	$(gallery[currentPictureIndex]).show();
 
-	$('.images').click(function () {
+	$('#a-right').click(function () {
 		$(gallery[currentPictureIndex]).fadeOut(200, function(){
 		
 		if(currentPictureIndex === gallery.length - 1){
@@ -14,9 +57,21 @@ $(document).ready(function (){
 		$(gallery[currentPictureIndex]).fadeIn(100);
 		});
 	})
+
+	$(gallery[currentPictureIndex]).show();
+
+	$('#a-left').click(function () {
+		$(gallery[currentPictureIndex]).fadeOut(200, function(){
+		
+		if(currentPictureIndex === 0){
+			currentPictureIndex = gallery.length -1 ;
+		} else{
+			currentPictureIndex--;
+		}
+		$(gallery[currentPictureIndex]).fadeIn(100);
+		});
+	})
 });
-
-
 
 
 
