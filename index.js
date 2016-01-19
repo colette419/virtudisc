51a7f191d08cc39097f41ceb5cf7d57faf2e07c6
Ashlee <ashleemariebrown93@gmail.com>
2016-01-19 10:58:22 +0100

    added hide & show navbar

--- index.js
+++ index.js
@@ -1,45 +1,21 @@
-// $(document).ready(function (){
-// 	// var button = $('#a-right')
-// 	// var gallery = $('.gallery'); 
-// 	// var currentPictureIndex = 0;
-// 	// $(gallery[currentPictureIndex]).show();
-
-// 	// $('#a-right').click(function () {
-// 	// 	$(gallery[currentPictureIndex]).fadeOut(200, function(){
-		
-// 	// 	if(currentPictureIndex === gallery.length - 1){
-// 	// 		currentPictureIndex = 0;
-// 	// 	} else{
-// 	// 		currentPictureIndex++;
-// 	// 	}
-// 	// 	$(gallery[currentPictureIndex]).fadeIn(100);
-// 	// 	});
-// 	// })
-
-// 	var button = $('#a-left')
-// 	var gallery = $('.gallery'); 
-// 	var currentPictureIndex = 0;
-// 	$(gallery[currentPictureIndex]).show();
-
-// 	$('#a-left').click(function () {
-// 		$(gallery[currentPictureIndex]).fadeOut(200, function(){
-		
-// 		if(currentPictureIndex === 0){
-// 			currentPictureIndex = gallery.length -1 ;
-// 		} else{
-// 			currentPictureIndex--;
-// 		}
-// 		$(gallery[currentPictureIndex]).fadeIn(100);
-// 		});
-// 	})
-// });
-
-
-//If the right button is clicked, do this
-//Else if the left button is clicked do this
-
-
 $(document).ready(function (){
+
+	var mbutton1 = $('#menu-buttonbeg');
+	var mbutton2 = $('#menu-buttoneng');
+	var nav = $('.navigation');
+	var currentNavIndex = 0;
+
+	$(nav[currentNavIndex]).show();
+
+	$('#menu-buttonbeg').click(function(){
+		$(nav).toggle('slide');
+	} )
+
+	$('#menu-buttonend').click(function(){
+		$(nav).toggle('slide');
+	} )
+
+
 	var buttonr = $('#a-right');
 	var buttonl = $('#a-left')
 	var gallery = $('.gallery'); 
@@ -71,6 +47,7 @@ $(document).ready(function (){
 		$(gallery[currentPictureIndex]).fadeIn(100);
 		});
 	})
+
 });
 
 
